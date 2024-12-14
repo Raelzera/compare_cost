792fc03e2c2a3d51ae4feb211b66d5dc5b60cc17
Raelzera <yraelenri@gmail.com>
2024-12-14 11:38:37 -0300

    Melhorias no código utilizando boas práticas de POO

--- compare_cost.py
+++ compare_cost.py
@@ -1,54 +1,73 @@
 import boto3
 
-def obter_tamanhos_discos():
-    ec2 = boto3.client('ec2')
-    response = ec2.describe_volumes()
+class EC2VolumeAnalyzer:
+    def __init__(self):
+        self.ec2_client = boto3.client('ec2')
+        self.gp2_size_gb = 0
+        self.io1_size_gb = 0
+        self.gp3_size_gb = 0
+        self.iops_io1 = 0
 
-    gp2_size_gb = 0
-    io1_size_gb = 0
-    gp3_size_gb = 0
-    iops_io1 = 0
+    def fetch_volumes(self):
+        return self.ec2_client.describe_volumes()
 
-    for volume in response['Volumes']:
-        tamanho_gb = volume['Size']
+    def analyze_volumes(self):
+        response = self.fetch_volumes()
 
-        if 'VolumeType' in volume:
-            if volume['VolumeType'] == 'gp2':
+        for volume in response['Volumes']:
+            size_gb = volume['Size']
+            volume_type = volume.get('VolumeType', '')
+
+            if volume_type == 'gp2':
                 iops_gp2 = volume.get('Iops', 0)
                 if iops_gp2 <= 3000: 
-                    gp2_size_gb += tamanho_gb
-            elif volume['VolumeType'] == 'io1':
-                io1_size_gb += tamanho_gb
-                iops_io1 += volume['Iops']
-            elif volume['VolumeType'] == 'gp3':
-                gp3_size_gb += tamanho_gb
+                    self.gp2_size_gb += size_gb
+            elif volume_type == 'io1':
+                self.io1_size_gb += size_gb
+                self.iops_io1 += volume.get('Iops', 0)
+            elif volume_type == 'gp3':
+                self.gp3_size_gb += size_gb
 
-    return gp2_size_gb, io1_size_gb, gp3_size_gb, iops_io1
-
-def calcular_custo(gp2_size_gb, io1_size_gb, gp3_size_gb, iops_io1):
-
-    preco_gp2_por_gb = 0.1
-    preco_io1_por_gb = 0.125
-    preco_gp3_por_gb = 0.08
-    preco_io1_por_iops = 0.065
-
-    custo_gp2 = gp2_size_gb * preco_gp2_por_gb
-    custo_io1 = io1_size_gb * preco_io1_por_gb + iops_io1 * preco_io1_por_iops
-    custo_gp3 = gp3_size_gb * preco_gp3_por_gb
-
-    return custo_gp2, custo_io1, custo_gp3
-
-gp2_size_gb, io1_size_gb, gp3_size_gb, iops_io1 = obter_tamanhos_discos()
-custo_gp2, custo_io1, custo_gp3 = calcular_custo(gp2_size_gb, io1_size_gb, gp3_size_gb, iops_io1)
+    def get_volume_sizes(self):
+        return self.gp2_size_gb, self.io1_size_gb, self.gp3_size_gb, self.iops_io1
 
 
-print(f'Tamanho total para gp2: {gp2_size_gb} GB')
-print(f'Tamanho total para io1: {io1_size_gb} GB, IOPS: {iops_io1}')
-print(f'Tamanho total para gp3: {gp3_size_gb} GB')
+class VolumeCostCalculator:
+    def __init__(self):
+        self.prices = {
+            'gp2': 0.1,
+            'io1': 0.125,
+            'gp3': 0.08,
+            'io1_iops': 0.065
+        }
 
-print(f'\nCusto total para gp2: ${custo_gp2:.2f}')
-print(f'Custo total para io1: ${custo_io1:.2f}')
-print(f'Custo total para gp3: ${custo_gp3:.2f}')
+    def calculate_costs(self, gp2_size_gb, io1_size_gb, gp3_size_gb, iops_io1):
+        cost_gp2 = gp2_size_gb * self.prices['gp2']
+        cost_io1 = io1_size_gb * self.prices['io1'] + iops_io1 * self.prices['io1_iops']
+        cost_gp3 = gp3_size_gb * self.prices['gp3']
 
-valor_reduzido_io1_para_gp3 = custo_io1 - custo_gp3
-print(f'\nValor reduzido ao trocar de io1 para gp3: ${valor_reduzido_io1_para_gp3:.2f}')
+        return cost_gp2, cost_io1, cost_gp3
+
+
+def main():
+    analyzer = EC2VolumeAnalyzer()
+    analyzer.analyze_volumes()
+    gp2_size_gb, io1_size_gb, gp3_size_gb, iops_io1 = analyzer.get_volume_sizes()
+
+    calculator = VolumeCostCalculator()
+    custo_gp2, custo_io1, custo_gp3 = calculator.calculate_costs(gp2_size_gb, io1_size_gb, gp3_size_gb, iops_io1)
+
+    print(f'Tamanho total para gp2: {gp2_size_gb} GB')
+    print(f'Tamanho total para io1: {io1_size_gb} GB, IOPS: {iops_io1}')
+    print(f'Tamanho total para gp3: {gp3_size_gb} GB')
+
+    print(f'\nCusto total para gp2: ${custo_gp2:.2f}')
+    print(f'Custo total para io1: ${custo_io1:.2f}')
+    print(f'Custo total para gp3: ${custo_gp3:.2f}')
+
+    valor_reduzido_io1_para_gp3 = custo_io1 - custo_gp3
+    print(f'\nValor reduzido ao trocar de io1 para gp3: ${valor_reduzido_io1_para_gp3:.2f}')
+
+
+if __name__ == "__main__":
+    main()
